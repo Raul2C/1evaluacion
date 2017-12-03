3420d194375b04bcf9b058271c7ad50dcd9fc6a1
Raul2C <33655468+Raul2C@users.noreply.github.com>
2017-12-03 12:57:25 +0100

    Add files via upload

---
+++ sumadorselectivo.py
@@ -0,0 +1,8 @@
+def sumadorselectivo():
+    n=input("Hasta que número quieres sumar?")
+    suma=0
+    for cont in range(1,n+1,1):
+        suma=suma + cont
+        print"suma =", suma
+sumadorselectivo()
+    
